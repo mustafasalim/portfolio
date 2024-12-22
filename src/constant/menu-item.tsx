71cf5db491e1611e34a32e30526047cf85bd1a8a
Mustafa Salim <salimmustafa763@gmail.com>
2024-12-22 16:49:40 +0300

    fix

--- src/constant/menu-item.tsx
+++ src/constant/menu-item.tsx
@@ -35,7 +35,7 @@ export const links = [
     icon: (
       <IconBrandLinkedin className="h-full w-full text-neutral-500 dark:text-neutral-300" />
     ),
-    href: "https://www.linkedin.com/in/mustafa-salim-562145205",
+    href: "https://www.linkedin.com/in/mustafasalim16/",
   },
   {
     title: "GitHub",
